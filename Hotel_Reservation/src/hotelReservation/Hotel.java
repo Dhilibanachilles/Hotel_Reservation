7d9dbd75dd5362c88814459a40e66efcb9318d27
dhiliban <dhiliban.baskar@gmail.com>
2021-03-16 12:01:46 +0530

    Added:UC5

--- Hotel_Reservation/src/hotelReservation/Hotel.java
+++ Hotel_Reservation/src/hotelReservation/Hotel.java
@@ -1,16 +1,20 @@
 package hotelReservation;
 
-import java.text.ParseException;
-
 public class Hotel {
     private final String name;
     private double regularWeekdayRates;
     private double regularWeekendRates;
+    private final int rating;
 
-    public Hotel(String name, double regularWeekdayRates, double regularWeekendRates) {
+    public Hotel(String name, double regularWeekdayRates, double regularWeekendRates, int rating) {
         this.name = name;
         this.regularWeekdayRates = regularWeekdayRates;
         this.regularWeekendRates = regularWeekendRates;
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
     }
 
     public double getRegularWeekendRates() {
@@ -33,13 +37,11 @@ public class Hotel {
         this.regularWeekdayRates = regularWeekdayRates;
     }
 
-    public double getPrice(String... dates) throws ParseException {
-        double rate = 0.0;
-        for (String date : dates) {
-            if (DateUtil.isWeekend(date))
-                rate += regularWeekendRates;
-            else rate += regularWeekdayRates;
-        }
+    public double getPrice(String start, String end) {
+        double rate;
+        rate = DateUtil.countSaturdaysAndSundays(start, end) * regularWeekendRates
+                + DateUtil.numberOfWeekdays(start, end) * regularWeekdayRates;
         return rate;
     }
+
 }
--- Hotel_Reservation/src/hotelReservation/HotelReservation.java
+++ Hotel_Reservation/src/hotelReservation/HotelReservation.java
@@ -1,9 +1,6 @@
 package hotelReservation;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class HotelReservation {
     private static final List<Hotel> hotelList = new ArrayList<>();
@@ -17,32 +14,25 @@ public class HotelReservation {
         return true;
     }
 
-    public static String getCheapestHotel(String... dates) {
+    public static String getCheapestBestRatedHotel(String start, String end) {
         Hotel cheapestHotel = hotelList.get(0);
-        double cheapestRate=0;
-        try {
-            cheapestRate = hotelList.get(0).getPrice(dates);
-            for (Hotel hotel : hotelList) {
-                if (hotel.getPrice(dates) < cheapestRate) {
-                    cheapestHotel = hotel;
-                    cheapestRate = hotel.getPrice(dates);
-                }
-            }
-        }catch (ParseException e){
-            e.printStackTrace();
+        double rate = cheapestHotel.getPrice(start, end);
+        for (Hotel hotel : hotelList) {
+            if (hotel.getPrice(start, end) < rate)
+                cheapestHotel = hotel;
         }
-        return cheapestHotel.getName() + ", Total Rates: $" + Math.round(cheapestRate);
+        return cheapestHotel.getName() + ", Price: " + rate;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to hotel reservation program");
-        addHotel(new Hotel("Lakewood", 110, 90));
-        addHotel(new Hotel("Bridgewood", 150, 50));
-        addHotel(new Hotel("Ridgewood", 220, 150));
+        addHotel(new Hotel("Lakewood", 110, 90, 3));
+        addHotel(new Hotel("Bridgewood", 150, 50, 4));
+        addHotel(new Hotel("Ridgewood", 220, 150, 5));
         Scanner sc = new Scanner(System.in);
-        String[] dates = sc.nextLine().split(",");
-
-        System.out.println(getCheapestHotel(dates));
+        String[] dates = sc.nextLine().replaceAll(" ", "").split(",");
+        sc.close();
+        System.out.println(getCheapestBestRatedHotel(dates[0], dates[1]));
 
     }
 }
